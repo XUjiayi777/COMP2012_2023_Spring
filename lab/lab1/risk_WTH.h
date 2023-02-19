1f308bfac2a8c30ae15715e1628e84031397612a
XUjiayi777 <jxugl777@gmail.com>
2023-02-19 12:07:09 +0800

    edit risk_with.h

--- lab/lab1/risk_WTH.h
+++ lab/lab1/risk_WTH.h
@@ -7,12 +7,12 @@
 using namespace std;
 
 /* Constant definitions */
-const char* TEST_NAME = "WTH (Waist to Hip) Ratio";
+const char *TEST_NAME = "WTH (Waist to Hip) Ratio";
 const float FEMALE_THRESHOLD1 = 0.81;
 const float FEMALE_THRESHOLD2 = 0.85;
 const float MALE_THRESHOLD1 = 0.96;
 const float MALE_THRESHOLD2 = 1.0;
 
-const char* BODY_SHAPE[3] = { "PEAR","AVOCADO",  "APPLE" };
+const char *BODY_SHAPE[3] = {"PEAR", "AVOCADO", "APPLE"};
 
 #endif
