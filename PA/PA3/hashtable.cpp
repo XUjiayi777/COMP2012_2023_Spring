834a86047355ed8e4031313cb3bd6701781833cc
XUjiayi777 <jxugl777@gmail.com>
2023-05-03 21:32:44 +0800

    finish task after chaecking memory leak

--- PA/PA3/hashtable.cpp
+++ PA/PA3/hashtable.cpp
@@ -1,5 +1,4 @@
 #include "hashtable.h"
-
 BoardOptimalMove BoardHashTable::getHashedMove(const unsigned long long id, const int depth)
 {
     int index = id % TABLE_SIZE;
@@ -31,6 +30,7 @@ BoardOptimalMove BoardHashTable::getHashedMove(const unsigned long long id, cons
 void BoardHashTable::updateTable(const unsigned long long id, const int depth, const BoardOptimalMove &optimalMove)
 {
     int index = id % TABLE_SIZE;
+    int flag = 0;
     if (table[index] == nullptr)
     {
         table[index] = new BoardHashNode(id, depth, optimalMove);
@@ -51,23 +51,37 @@ void BoardHashTable::updateTable(const unsigned long long id, const int depth, c
                 {
                     cur->depth = depth;
                     cur->optimalMove = optimalMove;
+                    flag = 1;
                     return;
                 }
             }
         }
-        cur = new BoardHashNode(id, depth, optimalMove);
+        if (flag == 1)
+        {
+            return;
+        }
+        cur = table[index];
+        table[index]=new BoardHashNode(id, depth, optimalMove);
+        table[index]->next=cur;
+        cur = nullptr;
     }
 }
 
-void BoardHashTable::clearTable(){
-    for (int i =0;i<TABLE_SIZE;i++){
-        if (table[i]!=nullptr){
+void BoardHashTable::clearTable()
+{
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        if (table[i] != nullptr)
+        {
             BoardHashNode *cur = table[i];
-            while(cur!=nullptr){
-                BoardHashNode * temp = cur;
-                cur= cur->next;
+            while (cur != nullptr)
+            {
+                BoardHashNode *temp = cur;
+                cur = cur->next;
                 delete temp;
+                temp = nullptr;
             }
+            table[i] = nullptr;
         }
     }
 }
